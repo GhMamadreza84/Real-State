31f24c03d711566046d8132a380650f3e4eece3b
ghmamadreza2 <ghmamadreza84@gmail.com>
2025-01-27 19:54:21 +0330

    i handle login user

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,35 +1,32 @@
-import { CredentialProvider } from "next-auth/providers";
-import { veryfiPassword } from "@/utils/auth";
-import User from "@/models/User";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "@/models/User";
 import connectDB from "@/utils/connectDB";
+import { veryfiPassword } from "@/utils/auth";
 
-const authOptions = {
-  session: {
-    strategy: "jwt",
-  },
+export const authOptions = {
+  session: { strategy: "jwt" },
   providers: [
-    CredentialProvider({
+    CredentialsProvider({
       async authorize(credentials) {
         const { email, password } = credentials;
 
         try {
           await connectDB();
-        } catch (err) {
-          throw new Error("Internal server error");
+        } catch (error) {
+          throw new Error("مشکلی در سرور رخ داده است");
         }
 
-        if (!email || !password) {
-          throw new Error("Please enter valid data");
-        }
+        if (!email || !password)
+          throw new Error("لطفا اطلاعات معتبر وارد کنید");
 
-        const user = User.findOne({ email });
+        const user = await User.findOne({ email });
 
-        if (!user) throw new Error("Please first create user");
+        if (!user) throw new Error("لطفا ابتدا حساب کاربری ایجاد کنید");
 
         const isValid = await veryfiPassword(password, user.password);
 
-        if (!isValid) throw new Error("Email or password is incorrect ");
+        if (!isValid) throw new Error("ایمیل یا رمز عبور اشتباه است");
 
         return { email };
       },
@@ -38,4 +35,5 @@ const authOptions = {
 };
 
 const handler = NextAuth(authOptions);
-export { handler as POST, handler as GET };
+
+export { handler as GET, handler as POST };
