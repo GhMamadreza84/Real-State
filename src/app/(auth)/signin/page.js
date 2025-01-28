7a6a5221cb9629842ea8eb028366573e036bff3f
ghmamadreza2 <ghmamadreza84@gmail.com>
2025-01-28 11:56:32 +0330

    i now secure the sign in page and signup page when user authenticated cant enter the this page

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -1,11 +1,12 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 import SigninPage from "@/template/SigninPage";
 
 const Signin = async () => {
   const session = await getServerSession(authOptions);
-  console.log(session);
-    
+  if (session) redirect("/");
+
   return <SigninPage />;
 };
 
--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -1,6 +1,11 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 import SignupPage from "@/template/SignupPage";
 
-const Signup = () => {
+const Signup = async () => {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/");
   return <SignupPage />;
 };
 
