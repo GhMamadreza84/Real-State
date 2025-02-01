232bae3476a317548efa66c26795a2dfccbfab39
ghmamadreza2 <ghmamadreza84@gmail.com>
2025-02-01 19:46:54 +0330

    i now develope the profile api and now going for testing it

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,85 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+import { Types } from "mongoose";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+    const body = await req.json();
+    console.log(body);
+    const {
+      title,
+      description,
+      location,
+      phone,
+      realState,
+      price,
+      constructionDate,
+      amenities,
+      rules,
+      category,
+    } = body;
+    const session = await getServerSession(req);
+    console.log(session);
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "لطفا وارد حساب کاربری خود شوید",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+
+    if (
+      !title ||
+      !location ||
+      !description ||
+      !phone ||
+      !realState ||
+      !price ||
+      !constructionDate ||
+      !category
+    ) {
+      return NextResponse.json(
+        { error: "لطفا اطلاعت معتبر وارد کنید" },
+        { status: 400 }
+      );
+    }
+
+    const newProfile = await Profile.create({
+      title,
+      description,
+      location,
+      realState,
+      constructionDate,
+      amenities,
+      rules,
+      category,
+      price: +price,
+      userId: new Types.ObjectId(user._id),
+    });
+    console.log(newProfile);
+    return NextResponse.json(
+      { message: "آگهی جدید اضافه شد" },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
