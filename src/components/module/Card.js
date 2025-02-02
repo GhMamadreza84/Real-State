b31ff4ea143840edab1fbe72943745a7eb6b86b1
ghmamadreza2 <ghmamadreza84@gmail.com>
2025-02-02 15:13:16 +0330

    i now design the card component and show values in this

--- src/components/module/Card.js
+++ src/components/module/Card.js
@@ -1,5 +1,43 @@
-const Card = ({ data }) => {
-  return <div>{data.title}</div>;
+import styles from "@/module/Card.module.css";
+import { MdApartment } from "react-icons/md";
+import { RiHome3Line } from "react-icons/ri";
+import { BiLeftArrowAlt, BiStore } from "react-icons/bi";
+import { GiOfficeChair } from "react-icons/gi";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { e2p } from "@/utils/replaceNumber";
+import Link from "next/link";
+
+const getIconForCategory = (category) => {
+  switch (category) {
+    case "apartment":
+      return <MdApartment />;
+    case "villa":
+      return <RiHome3Line />;
+    case "store":
+      return <BiStore />;
+    case "office":
+      return <GiOfficeChair />;
+    default:
+      return null;
+  }
+};
+
+const Card = ({ data: { category, title, location, price } }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.icon}>{getIconForCategory(category)}</div>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.location}>
+        <HiOutlineLocationMarker />
+        {location}
+      </p>
+      <span>{e2p(price.toLocaleString())} تومان</span>
+      <Link href="/">
+        مشاهده آگهی
+        <BiLeftArrowAlt />
+      </Link>
+    </div>
+  );
 };
 
 export default Card;
