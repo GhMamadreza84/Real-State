d4322ab137fdf34219218ba3c5e8aba3d27e91f1
ghmamadreza2 <ghmamadreza84@gmail.com>
2025-02-17 17:42:25 +0330

    i get data from profile api and pass to buy-residential component

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,7 +1,12 @@
 import BuyResidentialPage from "@/template/BuyResidentialPage";
 
-const BuyResidential = () => {
-  return <BuyResidentialPage />;
+const BuyResidential = async () => {
+  // This method is not recommended use it only when your API route is a foreign API route.
+  const res = await fetch("http://localhost:3000/api/profile", {
+    cache: "no-store",
+  });
+  const data = await res.json();
+  return <BuyResidentialPage data={data} />;
 };
 
 export default BuyResidential;
