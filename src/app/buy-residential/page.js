07599997ab797101bdda2806f47670f5f63bbdf0
ghmamadreza2 <ghmamadreza84@gmail.com>
2025-02-19 16:07:57 +0330

    i filter data by search params and pass to buyresidential component

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,13 +1,20 @@
 import BuyResidentialPage from "@/template/BuyResidentialPage";
 
-const BuyResidential = async () => {
+const BuyResidential = async ({ searchParams }) => {
   // This method is not recommended use it only when your API route is a foreign API route.
   const res = await fetch("http://localhost:3000/api/profile", {
     cache: "no-store",
   });
   const data = await res.json();
+
   if (data.error) return <h3>مشکلی پیش آمده است</h3>;
-  return <BuyResidentialPage data={data.data} />;
+
+  let finalData = data.data;
+  if (searchParams.category) {
+    finalData = finalData.filter((i) => i.category === searchParams.category);
+  }
+
+  return <BuyResidentialPage data={finalData} />;
 };
 
 export default BuyResidential;
