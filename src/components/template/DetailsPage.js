65193963fc9c3068d10f5d99925edefe76dd30ff
ghmamadreza2 <ghmamadreza84@gmail.com>
2025-03-15 19:00:08 +0330

    i now start the sidebar section

--- src/components/template/DetailsPage.js
+++ src/components/template/DetailsPage.js
@@ -5,18 +5,30 @@ import { BiCalendarCheck, BiStore } from "react-icons/bi";
 import { RiHome3Line } from "react-icons/ri";
 import { MdApartment } from "react-icons/md";
 import { GiOfficeChair } from "react-icons/gi";
+import { e2p, sp } from "@/utils/replaceNumber";
 import styles from "@/template/DetailsPage.module.css";
 import ItemList from "@/module/ItemList";
 import Title from "@/module/Title";
-const DetailsPage = ({ data }) => {
+const DetailsPage = ({
+  data: {
+    title,
+    location,
+    description,
+    amenities,
+    rules,
+    realState,
+    phone,
+    price,
+    category,
+    constructionDate,
+  },
+}) => {
   const categories = {
     aparatment: "آپارتمان",
     villa: "ویلا",
     store: "مفازه",
     office: "دفتر",
   };
-
-  console.log(data);
 
   const icons = {
     aparatment: <MdApartment />,
@@ -27,19 +39,38 @@ const DetailsPage = ({ data }) => {
   return (
     <div className={styles.container}>
       <div className={styles.main}>
-        <h1>{data.title}</h1>
+        <h1>{title}</h1>
         <span>
           <HiOutlineLocationMarker />
-          {data.location}
+          {location}
         </span>
         <Title>توضیحات</Title>
-        <p>{data.description}</p>
+        <p>{description}</p>
         <Title>امکانات</Title>
-        <ItemList data={data.amenities} />
+        <ItemList data={amenities} />
         <Title>قوانین</Title>
-        <ItemList data={data.rules} />
+        <ItemList data={rules} />
       </div>
-      <div className={styles.sidebar}></div>
+      <div className={styles.sidebar}>
+        <div className={styles.realState}>
+          <SiHomebridge />
+          <p>املاک {realState}</p>
+          <span>
+            <AiOutlinePhone /> {e2p(phone)}
+          </span>
+        </div>
+        <div className={styles.price}>
+          <p>
+            {icons[category]}
+            {categories[category]}
+          </p>
+          <p>{sp(price)} تومان</p>
+          <p>
+            <BiCalendarCheck />
+            {new Date(constructionDate).toLocaleDateString("fa-IR")}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
