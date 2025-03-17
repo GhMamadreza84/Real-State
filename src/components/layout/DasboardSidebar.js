2c55dc8c3ebb3dd427461122c231c22308c29af4
ghmamadreza2 <ghmamadreza84@gmail.com>
2025-03-17 12:01:24 +0330

    i now customize the admin panel , now going to develope the new route for amdin

--- src/components/layout/DasboardSidebar.js
+++ src/components/layout/DasboardSidebar.js
@@ -5,17 +5,18 @@ import styles from "@/layout/DasboardSidebar.module.css";
 import Link from "next/link";
 import LogoutButton from "@/module/LogoutButton";
 
-const DasboardSidebar = async ({ children }) => {
-  const session = await getServerSession(authOptions);
+const DasboardSidebar = async ({ children, email, role }) => {
   return (
     <div className={styles.container}>
       <div className={styles.sidebar}>
         <CgProfile />
-        <p>{session?.user.email}</p>
+        <h4>{role === "ADMIN" ? "ادمین" : null}</h4>
+        <p>{email}</p>
         <span></span>
         <Link href="/dashboard">حساب کاربری</Link>
         <Link href="/dashboard/my-profiles">آگهی های من</Link>
         <Link href="/dashboard/add">ثبت آگهی</Link>
+        {role === "ADMIN" ? <Link href="/admin">در انتظار تایید</Link> : null}
         <LogoutButton />
       </div>
       <div className={styles.main}>{children}</div>
