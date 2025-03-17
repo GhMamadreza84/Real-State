773d85a848e87ad3450f734fa795a1f207505784
ghmamadreza2 <ghmamadreza84@gmail.com>
2025-03-17 11:44:15 +0330

    i now get user information and pass the role and email of information to dashboard side bar for customize the admin panel

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,11 +2,23 @@ import DasboardSidebar from "@/layout/DasboardSidebar";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import connectDB from "@/utils/connectDB";
+import User from "@/models/User";
 
 const DashboardLayout = async ({ children }) => {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/signin");
-  return <DasboardSidebar>{children}</DasboardSidebar>;
+
+  await connectDB();
+  const user = await User.findOne({ email: session.user.email });
+
+  if (!user) return <h3>مشکلی پیش آمده است</h3>;
+
+  return (
+    <DasboardSidebar role={user.role} email={user.email}>
+      {children}
+    </DasboardSidebar>
+  );
 };
 
 export default DashboardLayout;
