9ae52df027f7f7f1358531701dd0768d00baebe4
ghmamadreza2 <ghmamadreza84@gmail.com>
2025-03-17 14:18:12 +0330

    admin panel complated

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,8 +1,11 @@
-import connectDB from "@/utils/connectDB";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import connectDB from "@/utils/connectDB";
 import User from "@/models/User";
+import DasboardSidebar from "@/layout/DasboardSidebar";
+import AdminPage from "@/template/AdminPage";
+import Profile from "@/models/Profile";
 
 const Admin = async () => {
   await connectDB();
@@ -11,7 +14,13 @@ const Admin = async () => {
 
   const user = await User.findOne({ email: session.user.email });
   if (!user.role === "ADMIN") redirect("/dashboard");
-  return <div>Admin</div>;
+
+  const profiles = await Profile.find({ published: false });
+  return (
+    <DasboardSidebar role={user.role} email={user.email}>
+      <AdminPage profiles={profiles} />
+    </DasboardSidebar>
+  );
 };
 
 export default Admin;
